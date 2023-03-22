fe3aea10648162233327c2fe2b396148db1d1fd3
codeitpratibha <pratibha.75444@purc.tu.edu.np>
2023-03-22 15:00:34 +0545

    Inheritance

---
+++ todo_user/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name':'Multiuser To-Do',
+    'description':'Extend the To-Do app to multiuser',
+    'author':'Pratibha Kulung',
+    'depends':['to_do','mail'],
+    'data':['views/todo_task.xml'],
+    
+
+}
